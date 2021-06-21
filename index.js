40ea68d7087f7f2c1d0fd5d86facbe2e8e324734
root <root@l65046388.adm.unifor.br>
2021-06-21 13:39:10 -0300

    Criando as primeiras rotas

--- index.js
+++
@@ -1,4 +0,0 @@
-function enviarEmail(nome, email) {
-    console.log("Enviar email para " + nome + " com o email " + email);
-}
-enviarEmail("Paulo", "nagpaulo@gmail.com");
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
-import express from "express";
+import express, { request, response } from "express";
 const app = express();
 
-app.listen('3000', () => console.log('Server is running!'));
+app.get('/test', (request, response) => {
+  return response.send("Ola! NLW");
+});
+
+app.post("/test-post", (request, response) => {
+  return response.send("Ola! NLW - POST!")
+});
+
+app.listen('3000', () => console.log('Server is running!!'));
