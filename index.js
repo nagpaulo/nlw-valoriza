ad110914bcba9d56e9b38ade34fad3f6345cb170
root <root@l65046388.adm.unifor.br>
2021-06-21 13:02:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log("Enviar email para " + nome + " com o email " + email);
+}
+enviarEmail("Paulo", "nagpaulo@gmail.com");
---
+++ index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(nome:string, email: string) {
+  console.log(`Enviar email para ${nome} com o email ${email}`);
+}
+
+enviarEmail("Paulo", "nagpaulo@gmail.com");
